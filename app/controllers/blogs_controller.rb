8b09f27c26bef13872dba1e1697d2d3a71be8b37
takumi20210430 <uniquetime.t@gmail.com>
2021-04-05 08:32:09 +0000

    [Finish]3-4

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,11 @@ class BlogsController < ApplicationController
 
   def show
   end
+  
+  def index
+    @blogs = Blog.all
+  end
+  
 
   def new
     @blog = Blog.new
@@ -12,7 +17,8 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     blog.save
-    redirect_to volgs_path
+    redirect_to blogs_path
+    
   end
 
   def edit
